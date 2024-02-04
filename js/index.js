0efd9ef7d1468b5a5cc6412657c96fdd3b30a094
Benjamin V <you@example.com>
2024-02-04 00:18:26 -0300

    Adicionado section about_me

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-$('nav div a').click(function (e) {
-    e.preventDefault();
-})
+function showText(text) {
+    console.log('clicou na imagem!');
+    var displayText = document.getElementById('displayText');
+    displayText.innerText = text;
+
+    //show text and hide others
+    displayText.classList.remove('hidden_text');
+
+}
