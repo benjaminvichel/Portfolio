8c7ade5dff42eb8d2ea3114c86a3341d3356aa18
Benjamin V <you@example.com>
2024-02-06 03:11:57 -0300

    mudanças layout sections 'about me' e 'project'!

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-function showText(text) {
+function showDiv() {
     console.log('clicou na imagem!');
-    var displayText = document.getElementById('displayText');
-    displayText.innerText = text;
+
+    var displayDiv = document.getElementById();
 
     //show text and hide others
-    displayText.classList.remove('hidden_text');
+    displayDiv.classList.remove('hidden_div');
 
+}
+
+
+function showMessage(message) {
+    event.preventDefault();
+    var messageContainer = document.getElementById("messageContainer")
+    switch (message) {
+        case 'certificate':
+            messageContainer.innerHTML = "<p>Aqui está o seu certificado!</p>"
+            break;
+        case 'outraMensagem1':
+            messageContainer.innerHTML = "<p>Outra mensagem 1!</p>";
+            break;
+        case 'outraMensagem2':
+            messageContainer.innerHTML = "<p>Outra mensagem 2!</p>";
+            break;
+        // Adicione mais casos conforme necessário
+        default:
+            messageContainer.innerHTML = "<p>Mensagem padrão!</p>";
+    }
 }
