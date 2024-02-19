eaf3aa215d51c1dff2d0eafea2121eff1c65655e
Benjamin <benni.vichel@hotmail.com>
2024-02-19 06:41:34 +0100

    added dark mode option in nav bar

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none';
 }
+//Start darkMode
+function toggleDarkMode() {
+    document.body.classList.toggle('dark');
+    document.querySelectorAll('section.portfolio, section.especialidades, .especialidades .especialidades-box, h2.titles').forEach(section => {
+        section.classList.toggle('dark');
+    });
+}
+//darkMode outside sideBar
+const chk = document.getElementById('chk');
+chk.addEventListener('change', toggleDarkMode);
+
+//darkMode in sideBar
+const chkSideBar = document.getElementById('chkSideBar');
+chkSideBar.addEventListener('change', toggleDarkMode);
+
+//End darkMode
